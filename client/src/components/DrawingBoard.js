29d4cbf1b4375ce571a7eeb50b837f18d61c3af7
rachidelaid <rachidelaid@gmail.com>
2022-10-22 15:49:56 +0100

    fix board resize

--- client/src/components/DrawingBoard.js
+++ client/src/components/DrawingBoard.js
@@ -2,32 +2,38 @@ import React, { useEffect, useRef, useState } from 'react';
 import Drawing, { brushArc } from 'react-drawing';
 
 export default function DrawingBoard() {
-  const [canvas, setCanvas] = useState({ width: 500, height: 500 });
+  const [canvas, setCanvas] = useState({ width: null, height: null });
 
   const canvasWrap = useRef();
+  console.log('rendering DrawingBoard');
 
-  window.addEventListener('resize', () => {
+  const reziseCanvas = () => {
+    console.log('reziseCanvas');
     setCanvas({
       width: canvasWrap.current?.getClientRects()[0].width,
       height: canvasWrap.current?.getClientRects()[0].height,
     });
-  });
+  };
 
   useEffect(() => {
-    setCanvas({
-      width: canvasWrap.current?.getClientRects()[0].width,
-      height: canvasWrap.current?.getClientRects()[0].height,
-    });
+    console.log('canvas');
+    if (!canvas.width && canvasWrap.current) {
+      reziseCanvas();
+    }
+    window.addEventListener('resize', reziseCanvas);
+
+    return () => {
+      window.removeEventListener('resize', reziseCanvas);
+    };
   }, [canvasWrap]);
 
   return (
-    <div ref={canvasWrap} className='vh-100 my-3 border'>
+    <div ref={canvasWrap} className="vh-100 my-3 border">
       <Drawing
         width={canvas.width}
         height={canvas.height}
         brush={brushArc({ fillStyle: 'black', size: 5 })}
       />
     </div>
-
-  )
+  );
 }
